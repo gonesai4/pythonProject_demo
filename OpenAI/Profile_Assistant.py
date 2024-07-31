6dad3dad3ce4b28956c4004f15a855a6f6233f77
gonesai4 <gonesai4@gmail.com>
2024-07-31 11:00:47 +0530

    updated

--- OpenAI/Profile_Assistant.py
+++ OpenAI/Profile_Assistant.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="sk-proj-LCqPPEGdjkWaes3CYVvaT3BlbkFJOsUqs4RwVYMZfUZplnIN"
+    api_key="aghraghas"
 )
 
 assistant = client.beta.assistants.create(
